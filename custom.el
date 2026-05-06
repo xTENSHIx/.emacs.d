19c97ab933c99b2038041ee4e750d55e2171c860
Manuel <manuel@neuer.com>
2026-05-06 10:27:25 +0200

    Added avy keybinds and stuff
    fixed merge conflict

--- custom.el
+++ custom.el
@@ -5,7 +5,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("9df4b113170b630d3c00cf3e67c59ecf4f007487f199cc46d54fb30efc8b79ce"
+   '("4e9f0b64032736975e3a5bd5617c829510d5bf55687bf8e7ff90b0721274e032"
+     "5beb9cc517b24959e2ee7be47584270bbe11a7b210807fa419d41ede12174a26"
      default)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
