4be8f723a07e71115d5353c1c79b9121617f0ac8
xTENSHIx <73906704+xTENSHIx@users.noreply.github.com>
2026-06-02 19:13:52 +0200

    Remove custom.el
    Removed custom variables and faces settings from custom.el.

--- custom.el
+++ custom.el
@@ -1,17 +1,1 @@
-;;; -*- lexical-binding: t -*-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("ca2ce81d33e0b4bd0fdf20caefdde9cb617fec42eeeaf5cd79c80d630bd5cf6a"
-     "4e9f0b64032736975e3a5bd5617c829510d5bf55687bf8e7ff90b0721274e032"
-     default))
- '(warning-suppress-log-types '((native-compiler))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
